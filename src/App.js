e208f6ff52b2b7b8e043fbe44a4517c2b17363ea
makniahmed-31 <makni.ahmed@gmail.com>
2020-10-06 11:33:38 +0100

    V1

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React,{useState} from 'react';
+import './App.css';
+import Filter from './component/Filter';
+import MovieCard from './component/MovieCard';
+import MovieList from './component/MovieList';
+
+function App() {
+  const [movielist, setmovielist] = useState([
+    {title:'The Girl Next Door',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ibXBURUVjdnRrTmptRW1Dam1n.jpg',
+    rate:6},
+    {title:'Enola Holmes',
+    description:'Enola Holmes est un film de aventure américano-britannique réalisé par Harry Bradbeer sorti en 2020 sur le service Netflix',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZTE5wRXZOZWNjTkVtcHB2bUVtcGo.jpg',
+    rate:5}, 
+    {title:'Mulan',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZcG1tam12bWptYWNtbW1ETGNtWWJtdm1F.jpg',
+    rate:8},        
+    {title:'Tenet',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZVGptbW1ZWUVFY3ZOWUdObVBQbWo.jpg',
+    rate:4},
+    {title:'Project Power',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZTGNtcEVtWWpFY212bGNOYm12bXZtQnc.jpg',
+    rate:3},
+    {title:'The Girl Next Door',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ibXBURUVjdnRrTmptRW1Dam1n.jpg',
+    rate:2},
+    {title:'Enola Holmes',
+    description:'Enola Holmes est un film de aventure américano-britannique réalisé par Harry Bradbeer sorti en 2020 sur le service Netflix',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZTE5wRXZOZWNjTkVtcHB2bUVtcGo.jpg',
+    rate:1}, 
+    {title:'Mulan',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZcG1tam12bWptYWNtbW1ETGNtWWJtdm1F.jpg',
+    rate:8},        
+    {title:'Tenet',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZVGptbW1ZWUVFY3ZOWUdObVBQbWo.jpg',
+    rate:9},
+    {title:'Project Power',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZTGNtcEVtWWpFY212bGNOYm12bXZtQnc.jpg',
+    rate:4},
+    {title:'The Girl Next Door',
+    description:'A teenager is dreams come true when a former porn star moves in next door and they fall in love',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ibXBURUVjdnRrTmptRW1Dam1n.jpg',
+    rate:6},
+    {title:'Enola Holmes',
+    description:'Enola Holmes est un film de aventure américano-britannique réalisé par Harry Bradbeer sorti en 2020 sur le service Netflix',
+    posterUrl:'https://pic.egybest.net/i/WmFwZndlY21ZTE5wRXZOZWNjTkVtcHB2bUVtcGo.jpg',
+    rate:5},
+]);
+
+     const [Filtermovie, setFiltermovie] = useState("")
+
+  const FindMovie=(x)=>{
+    setFiltermovie(x);
+  }
+ const AddFn=(newMovie )=>{
+      setmovielist([...movielist,newMovie]);
+ }
+  return (
+    <div className="App">
+
+
+    <Filter FindMovie={FindMovie} />
+
+    <MovieCard AddFn={AddFn} />
+
+    <MovieList  movielist={movielist} Filtermovie={Filtermovie} />
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MovieList.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FiStar } from 'react-icons/fi';
+
+const MovieList = (props) => {
+    var movielist=props.movielist;
+
+
+    const Stars =[];
+    const rateFn=(x)=>{
+        for (let i = 0; i < x; i++) {
+            
+            Stars.push(<FiStar className='fi' key={Math.floor(Math.random() * 1000000)} color='red' fill='yellow'/>)
+        }
+        return Stars;
+
+    };
+    const rateClick=(x)=>{
+        let Star =[];
+        for (let i = 0; i < x; i++) {
+            
+            Star.push(< FiStar className='Fi' key={i} onClick={fillStrOn} />)
+        }
+        return Star;
+    };
+    const fillStrOn=(e)=>{
+        console.log(Stars.findIndex(el=>el===e.target))
+        e.target.style.fill='red';
+    }
+    return ( 
+
+        <div className="container-movielist">
+            {
+                movielist.filter(el=>el.title.toUpperCase().includes(props.Filtermovie.toUpperCase())).map((el,index)=>
+                <div className="list" key={index} >
+                    
+                    <p>{rateClick(10)}</p>
+                    <img src={el.posterUrl} alt="poster" className="poster"/>
+                    <h1 className="title">{el.title}</h1>
+                    <p className="infos">{el.description}</p>
+                    <p>{el.rate}{rateFn(el.rate)}</p>
+                </div>
+                    )
+                    
+            }
+
+            
+        </div>
+     );
+}
+ 
+export default MovieList;
